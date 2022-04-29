e23d93617047837bfcf062aa7dbc6dbaae78aa41
narodnik <s>
2022-04-29 11:37:39 +0200

    kill entr after closing zathura

--- mdauto.sh
+++ mdauto.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # This will load an md file that auto-re-renders when the md is changed
+READER="${PDF_READER:=zathura}"
 FILENAME="$(basename "$1")"
 OUTFILE="/tmp/${FILENAME%.md}.pdf"
 echo $1 | entr -rs "md2pdf \"$1\" \"$OUTFILE\"" &
-zathura --mode fullscreen $OUTFILE &> /dev/null
+entr_pid="$!"
+( $READER "$OUTFILE" ; rm -f "$OUTFILE" ) > /dev/null
+kill $entr_pid
 
