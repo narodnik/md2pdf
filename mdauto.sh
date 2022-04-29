a13c2b008f455a3a8d19bf0a8291a01c01e0a9e1
narodnik <s>
2022-04-29 17:39:07 +0200

    add fullscreen

--- mdauto.sh
+++ mdauto.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # This will load an md file that auto-re-renders when the md is changed
-READER="${PDF_READER:=zathura}"
+READER="${PDF_READER:=zathura --mode fullscreen}"
 FILENAME="$(basename "$1")"
 OUTFILE="/tmp/${FILENAME%.md}.pdf"
+md2pdf "$1" "$OUTFILE"
 echo $1 | entr -rs "md2pdf \"$1\" \"$OUTFILE\"" &
 entr_pid="$!"
 ( $READER "$OUTFILE" ; rm -f "$OUTFILE" ) > /dev/null
--- mdview.sh
+++ mdview.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-READER="${PDF_READER:=zathura}"
+READER="${PDF_READER:=zathura --mode fullscreen}"
 FILENAME="$(basename "$1")"
 OUTFILE="/tmp/${FILENAME%.md}.pdf"
 md2pdf "$1" "$OUTFILE"
