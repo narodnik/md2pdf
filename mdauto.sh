e235126f08c5eb9f92fbdbcd87113fa01b188e2d
narodnik <x@x.org>
2022-08-06 13:03:47 +0200

    add -n so you can nohup mdauto

--- mdauto.sh
+++ mdauto.sh
@@ -4,7 +4,7 @@ READER="${PDF_READER:=zathura --mode fullscreen}"
 FILENAME="$(basename "$1")"
 OUTFILE="/tmp/${FILENAME%.md}.pdf"
 md2pdf "$1" "$OUTFILE"
-echo $1 | entr -rs "md2pdf \"$1\" \"$OUTFILE\"" &
+echo $1 | entr -nrs "md2pdf \"$1\" \"$OUTFILE\"" &
 entr_pid="$!"
 ( $READER "$OUTFILE" ; rm -f "$OUTFILE" ) > /dev/null
 kill $entr_pid
