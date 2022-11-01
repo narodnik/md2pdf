1b595d583c8b382d91dc5cae520e259207aa235a
x <x@x>
2022-11-01 10:59:15 +0000

    mdview no rm

--- mdview.sh
+++ mdview.sh
@@ -3,4 +3,4 @@ READER="${PDF_READER:=zathura}"
 FILENAME="$(basename "$1")"
 OUTFILE="/tmp/${FILENAME%.md}.pdf"
 md2pdf "$1" "$OUTFILE"
-( $READER "$OUTFILE" ; rm -f "$OUTFILE" ) &> /dev/null
+( $READER "$OUTFILE" ) &> /dev/null
